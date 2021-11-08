8c09e65be87cc6094008f330f179b16c8a21b939
UcGeorge <petegeorge20005@gmail.com>
2021-11-08 04:01:31 +0100

    More stuff

--- src/app.py
+++ src/app.py
@@ -1,4 +1,18 @@
 from resources import etherscan, bscscan
+from PyInquirer import prompt
+from examples import custom_style_3
 
 if __name__ == '__main__':
-    pass
+    q_table = [
+        {
+            'type': 'input',
+            'qmark': '[?]',
+            'name': 'input',
+            'message': 'Enter location of input file'
+        }
+    ]
+    user_config = prompt(q_table, style=custom_style_3)
+    input_file_location = user_config['input']
+    address_list = open(input_file_location, "r").read().replace('\n\n', ',')
+    etherscan.get_balance(address_list)
+    bscscan.get_balance(address_list)
