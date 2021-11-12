f6f81984b4dcbc26284c0e356e4238197fb069a5
UcGeorge <petegeorge20005@gmail.com>
2021-11-12 10:49:21 +0100

    Update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,18 +1,48 @@
 import cfscrape
 from threading import Thread
 from typing import Any, Dict
+from requests import Request
 
 
 import http.client
+from bs4 import BeautifulSoup as soup
+
+BALANCE_SELECTOR = "#ContentPlaceHolder1_divSummary > div.row.mb-4 > div.col-md-6.mb-3.mb-md-0 > div > div.card-body > div:nth-child(1) > div.col-md-8"
 
 
-def get(host: str, url: str) -> str:
-    scraper = cfscrape.create_scraper()
-    _html = scraper.get(f"https://{host+url}").content.decode('UTF-8')
-    file_name = (host+url).replace('/', '-')
-    with open(f'error/{file_name}.html', "w", encoding='utf-8') as out_file:
-        out_file.write(_html)
-    return _html
+def get(host: str, url: str, error: bool = False) -> str:
+    if error:
+        print(f'[INFO] Getting online data in IUAM for {url}')
+        token, agent = cfscrape.get_tokens(
+            f"https://{host+url}", 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+        request = Request(url=f"https://{host+url}", cookies=token, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'})
+        _html = request.data
+        _soup = soup(_html, 'lxml')
+        eth_balance = _soup.select(BALANCE_SELECTOR)
+        try:
+            _ = eth_balance[0].text.split()[0]
+            return _html
+        except IndexError:
+            writeToFile(host+url, 'html', 'error', _html)
+            get(host, url, True)
+    else:
+        print(f'[INFO] Getting online data in NORMAL MODE for {url}')
+        scraper = cfscrape.create_scraper()
+        _html = scraper.get(f"https://{host+url}").content.decode('UTF-8')
+        _soup = soup(_html, 'lxml')
+        eth_balance = _soup.select(BALANCE_SELECTOR)
+        try:
+            _ = eth_balance[0].text.split()[0]
+            return _html
+        except IndexError:
+            writeToFile(host+url, 'html', 'error', _html)
+            get(host, url, True)
+
+
+def writeToFile(fileName: str, type: str, folder: str, content):
+    with open(f'{folder}/{fileName}.{type}', "w", encoding='utf-8') as out_file:
+        out_file.write(content)
 
 
 def combine(a: 'Dict[str, Any]', b: 'Dict[str, Any]') -> Dict[str, Any]:
